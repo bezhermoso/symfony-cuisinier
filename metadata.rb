05a9ebeb120371169b4b5c69922e7865742c8566
Bez Hermoso <bez@activelamp.com>
2014-05-05 17:54:45 -0700

    Added dependencies.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,18 @@
 name             'symfblog'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Bezalel Hermoso'
+maintainer_email 'bezalelhermoso@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures symfblog'
 long_description 'Installs/Configures symfblog'
 version          '0.1.0'
+
+#depends "artifact"
+depends "apt"
+depends "apache2"
+depends "nginx"
+depends "mysql"
+depends "php", "> 1.3.10"
+depends "ohai"
+depends "nodejs"
+depends "npm"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# artifact_deploy "symfblog" do
+  # version "1.0.0"
+  # artifact_location "http://dl.dropbox.com/u/symfblog.tar.gz"
+  # deploy_to "/srv/symfblog"
+  # owner "symfblog"
+  # group "symfblog"
+  # action :deploy
+# end
